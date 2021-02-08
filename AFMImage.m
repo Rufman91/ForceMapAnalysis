bb1b87e5ebdbf0b4456e29e1e0caf823e8ece540
Rufman91 <63786485+Rufman91@users.noreply.github.com>
2021-02-08 17:01:42 +0100

    Initial Commit
    Blank properties and constructor

---
+++ AFMImage.m
@@ -0,0 +1,27 @@
+classdef AFMImage < matlab.mixin.Copyable
+   
+    properties
+        % File and Header properties
+        Name
+    end
+    properties
+        % Image Data Properties
+    
+    end
+    properties
+        % Properties related to Image processing/segmenting/classification
+        
+    end
+    
+    methods
+        % Main methods of the class
+        
+        function obj = AFMImage(MapFullFile,DataFolder,TempID)
+            % Constructor of the class. Extracts Header properties as well
+            % as all available channel-data
+            
+            
+        end
+        
+    end
+end
