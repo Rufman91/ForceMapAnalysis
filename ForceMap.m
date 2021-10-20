ab66e1989a2ce9368052dcf8f6b783a283ccf6eb
maerci <79519194+maerci@users.noreply.github.com>
2021-10-20 13:06:50 +0200

    update fit

--- ForceMap.m
+++ ForceMap.m
@@ -1861,7 +1861,7 @@ classdef ForceMap < matlab.mixin.Copyable
 
                          % Function to fit force data 
                          %b(1) (max-min)/2 b(2) FFT b(3) first sign change b(4) mean
-                         fit = @(b,x)  b(1).*(sin(x*obj.SegFrequency{j} + 2*pi/b(3)));    
+                         fit = @(b,x)  b(1).*(sin(x*obj.SegFrequency{j} + 2*pi/b(2)));    
                          % Least-Squares cost function:
                          fcn = @(b) sum((fit(b,x) - obj.FilterF{i,j}).^2);       
                          % Minimise Least-Squares with estimated start values:
@@ -1875,7 +1875,7 @@ classdef ForceMap < matlab.mixin.Copyable
                          %p = polyfit(x,obj.Indentation{i,j},n);
                          
                          %b(1) (max-min)/2 b(2) FFT b(3) first sign change b(4) mean
-                         fit = @(a,x)  a(1).*(sin(x*obj.SegFrequency{j} + 2*pi/a(3)));    
+                         fit = @(a,x)  a(1).*(sin(x*obj.SegFrequency{j} + 2*pi/a(2)));    
                          % Least-Squares cost function:
                          fcn = @(a) sum((fit(a,x) - obj.FilterH{i,j}).^2);       
                          % Minimise Least-Squares with estimated start values:
@@ -1883,8 +1883,8 @@ classdef ForceMap < matlab.mixin.Copyable
                          % Spacing of time vector:
                          xpH = linspace(min(obj.InterpTimeH{j}),max(obj.InterpTimeH{j}),100000);
                         
-                         obj.SineVarsF{i,j}(2) = (2*pi)./obj.SegFrequency{j};
-                         obj.SineVarsH{i,j}(2) = (2*pi)./obj.SegFrequency{j};
+                         %obj.SineVarsF{i,j}(2) = (2*pi)./obj.SegFrequency{j};
+                         %obj.SineVarsH{i,j}(2) = (2*pi)./obj.SegFrequency{j};
                         % phase shift in radians of force and height sine
                         %obj.phaseFrad = obj.SineVarsF{i,j}(2)/obj.SineVarsF{i,j}(3);
                         %obj.phaseHrad = obj.SineVarsH{i,j}(2)/obj.SineVarsH{i,j}(3);
@@ -3808,8 +3808,8 @@ classdef ForceMap < matlab.mixin.Copyable
                         xpH = linspace(min(obj.InterpTimeH{j}),max(obj.InterpTimeH{j}),100000);
                         
                         %Y-values fitted sine of indentation and force:
-                        ypF = obj.SineVarsF{i,j}(1)*(sin((2*pi*xpF)./obj.SineVarsF{i,j}(2) + 2*pi/obj.SineVarsF{i,j}(3)));
-                        ypH = obj.SineVarsH{i,j}(1)*(sin((2*pi*xpH)./obj.SineVarsH{i,j}(2) + 2*pi/obj.SineVarsH{i,j}(3)));
+                        ypF = obj.SineVarsF{i,j}(1)*(sin(xpF*obj.SegFrequency{j} + 2*pi/obj.SineVarsF{i,j}(2)));
+                        ypH = obj.SineVarsH{i,j}(1)*(sin(xpH*obj.SegFrequency{j} + 2*pi/obj.SineVarsH{i,j}(2)));
                         
                         k = k + 1;
                          % time indentation
