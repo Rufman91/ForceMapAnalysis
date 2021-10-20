d2037f229c144d1e2df46b498b6a0443cf8f9f53
maerci <79519194+maerci@users.noreply.github.com>
2021-10-20 11:58:45 +0200

    Update Fit

--- ForceMap.m
+++ ForceMap.m
@@ -1861,12 +1861,12 @@ classdef ForceMap < matlab.mixin.Copyable
 
                          % Function to fit force data 
                          %b(1) (max-min)/2 b(2) FFT b(3) first sign change b(4) mean
-                         fit = @(b,x)  b(1).*(sin(2*pi*x./b(2) + 2*pi/b(3)));    
+                         fit = @(b,x)  b(1).*(sin(x*obj.SegFrequency{j} + 2*pi/b(3)));    
                          % Least-Squares cost function:
                          fcn = @(b) sum((fit(b,x) - obj.FilterF{i,j}).^2);       
                          % Minimise Least-Squares with estimated start values:
                          options = optimset('MaxFunEvals',10000);
-                         obj.SineVarsF{i,j} = fminsearch(fcn, [AmplitudeF;  PeriodF;  firstsignchangeF],options); 
+                         obj.SineVarsF{i,j} = fminsearch(fcn, [AmplitudeF;  obj.SegFrequency{j};  firstsignchangeF],options); 
                          % Spacing of time vector:
                          xpF = linspace(min(obj.InterpTimeF{j}),max(obj.InterpTimeF{j}),100000);
                          
@@ -1875,14 +1875,16 @@ classdef ForceMap < matlab.mixin.Copyable
                          %p = polyfit(x,obj.Indentation{i,j},n);
                          
                          %b(1) (max-min)/2 b(2) FFT b(3) first sign change b(4) mean
-                         fit = @(a,x)  a(1).*(sin(2*pi*x./a(2) + 2*pi/a(3)));    
+                         fit = @(a,x)  a(1).*(sin(x*obj.SegFrequency{j} + 2*pi/a(3)));    
                          % Least-Squares cost function:
                          fcn = @(a) sum((fit(a,x) - obj.FilterH{i,j}).^2);       
                          % Minimise Least-Squares with estimated start values:
-                         obj.SineVarsH{i,j} = fminsearch(fcn, [AmplitudeH;  PeriodH; firstsignchangeH]); 
+                         obj.SineVarsH{i,j} = fminsearch(fcn, [AmplitudeH;  obj.SegFrequency{j}; firstsignchangeH]); 
                          % Spacing of time vector:
                          xpH = linspace(min(obj.InterpTimeH{j}),max(obj.InterpTimeH{j}),100000);
                         
+                         obj.SineVarsF{i,j}(2) = obj.SegFrequency{j};
+                         obj.SineVarsH{i,j}(2) = obj.SegFrequency{j};
                         % phase shift in radians of force and height sine
                         %obj.phaseFrad = obj.SineVarsF{i,j}(2)/obj.SineVarsF{i,j}(3);
                         %obj.phaseHrad = obj.SineVarsH{i,j}(2)/obj.SineVarsH{i,j}(3);
