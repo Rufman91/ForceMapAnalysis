4f221a6e06cbd3c731dd7704f413dba46600028f
maerci <79519194+maerci@users.noreply.github.com>
2021-11-13 10:51:25 +0100

    Change 4

--- ForceMap.m
+++ ForceMap.m
@@ -1879,7 +1879,7 @@ classdef ForceMap < matlab.mixin.Copyable
                          %try
                              % Function to fit force data 
                              %b(1) (max-min)/2 b(2) FFT b(3) first sign change b(4) mean
-                             fit = @(b,x)  AmplitudeF.*(sin(2*pi*x*(obj.SegFrequency{j})^(-1) + 2*pi/b(3)));    
+                             fit = @(b,x)  AmplitudeF.*(sin(2*pi*x*(obj.SegFrequency{j})^(-1) + 2*pi/firstsignchangeF));    
                              % Least-Squares cost function:
                              fcn = @(b) sum((fit(b,x) - obj.FilterF{i,j}).^2);       
                              % Minimise Least-Squares with estimated start values:
@@ -1891,7 +1891,7 @@ classdef ForceMap < matlab.mixin.Copyable
                              %xpF = linspace(min(obj.InterpTimeF{j}),max(obj.InterpTimeF{j}),100000);
                              obj.SineVarsF{i,j}(1)= AmplitudeF;
                              obj.SineVarsF{i,j}(2)= (obj.SegFrequency{j})^(-1);
-                             %obj.SineVarsF{i,j}(3)= obj.firstsignchangeF;
+                             obj.SineVarsF{i,j}(3)= firstsignchangeF;
                          %catch
                          %    obj.SineVarsF{i,j}(1)= AmplitudeF;
                          %    obj.SineVarsF{i,j}(2)= (obj.SegFrequency{j})^(-1);
