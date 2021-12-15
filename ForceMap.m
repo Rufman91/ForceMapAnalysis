6dd6fe1dde01291c140b94287589ce2799e98f7c
maerci <79519194+maerci@users.noreply.github.com>
2021-12-15 13:24:02 +0100

    update

--- ForceMap.m
+++ ForceMap.m
@@ -4070,7 +4070,7 @@ classdef ForceMap < matlab.mixin.Copyable
                      if obj.SegFrequency{j} > 0
                         
                         Period = 2*pi/obj.SegFrequency{j};
-                        End = obj.TStart{j} + pi/obj.SegFrequency{j};
+                        End = obj.TStart{j} + 2/obj.SegFrequency{j};
                         x = linspace(obj.TStart{j},End);
                         %x= obj.SegTime{j};
                        
