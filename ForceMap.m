13204e6370082b3a7db08fd1b7fcf8fe29483123
maerci <79519194+maerci@users.noreply.github.com>
2022-01-20 23:27:18 +0100

    tryout shift

--- ForceMap.m
+++ ForceMap.m
@@ -4063,11 +4063,11 @@ classdef ForceMap < matlab.mixin.Copyable
                         
                         %Y-values fitted sine of indentation and force:
                         try
-                            ypF = obj.SineVarsF{i,j}(1)*(sin(2*pi*x.*obj.SineVarsF{i,j}(2) + obj.SineVarsF{i,j}(3))) + meanF;
-                            ypH = obj.SineVarsH{i,j}(1)*(sin(2*pi*x.*obj.SineVarsH{i,j}(2) + obj.SineVarsH{i,j}(3))) + meanH;
+                            ypF = obj.SineVarsF{i,j}(1)*(sin(2*pi*x.*obj.SineVarsF{i,j}(2) + obj.SineVarsF{i,j}(3)));
+                            ypH = obj.SineVarsH{i,j}(1)*(sin(2*pi*x.*obj.SineVarsH{i,j}(2) + obj.SineVarsH{i,j}(3)));
                             
-                            ypFtrend = ypF + obj.slopeF(i,j).*x - obj.shiftF(i,j);
-                            ypHtrend = ypH + obj.slopeH(i,j).*x - obj.shiftH(i,j);
+                            ypFtrend = ypF + obj.slopeF(i,j).*x + obj.shiftF(i,j);
+                            ypHtrend = ypH + obj.slopeH(i,j).*x + obj.shiftH(i,j);
                         catch
                             ypF = zeros(length(x),1);
                             ypH = zeros(length(x),1);
