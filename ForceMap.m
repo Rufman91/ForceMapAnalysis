e640e8dd5eb7af8495fb09c88db8666adac1e5c5
maerci <79519194+maerci@users.noreply.github.com>
2022-03-12 20:40:28 +0100

    c sine

--- ForceMap.m
+++ ForceMap.m
@@ -4022,6 +4022,9 @@ classdef ForceMap < matlab.mixin.Copyable
                         lF = obj.slopeF(i,j).*x;
                         lH = obj.slopeH(i,j).*x;
                         
+                        if obj.SegFrequency{j} = 0
+                            obj.FilterF{i,j} = zeros(length(x),1);
+                        end
 
                         
                         hold on
