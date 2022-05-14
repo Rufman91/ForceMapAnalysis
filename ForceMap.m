05bbd13d49297c457d77cf9a2d61f7235c46f2de
maerci <79519194+maerci@users.noreply.github.com>
2022-05-14 20:24:37 +0200

    try

--- ForceMap.m
+++ ForceMap.m
@@ -4516,6 +4516,7 @@ classdef ForceMap < matlab.mixin.Copyable
                 set(ax2,'units','normalized','position',[0.35 0.6 0.7 0.3]);
                 set(ax1,'xscale','log','xlim',[a e]);
                 set(ax2,'xlim',[x1 x2],'ytick',[],'yticklabel','','YColor','none');
+                axis([-1  1  a  150])
 
                 set([ax1 ax2],'box','off');
                 set(ax2,'yticklabel','','box','on');
@@ -4570,7 +4571,7 @@ classdef ForceMap < matlab.mixin.Copyable
                 set(ax3,'units','normalized','position',[0.05 0.1 0.3 0.3]);
                 set(ax4,'units','normalized','position',[0.35 0.1 0.7 0.3]);
                 set(ax3,'xscale','log','xlim',[a e]);
-                set(ax4,'xlim',[x1 150],'ytick',[],'yticklabel','','YColor','none');
+                set(ax4,'xlim',[x1 x2],'ytick',[],'yticklabel','','YColor','none');
 
                 set([ax3 ax4],'box','off');
                 set(ax4,'yticklabel','','box','on');
